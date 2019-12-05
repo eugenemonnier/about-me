38286cdf6beaea56c0915fa9b009340e29483ff6
EugeneMonnier <eugene.monnier@gmail.com>
2019-12-05 15:13:45 -0800

    lint cleanup!

--- js/app.js
+++ js/app.js
@@ -10,11 +10,11 @@ var allMyBeers = '';
 
 //make Read My Mind & Quiz links variables
 var mindGameLink = document.getElementById('mind');
-var quizLink = document.getElementById('quiz'); 
+var quizLink = document.getElementById('quiz');
 
 
 // Greet the user
-var greeting = 'Hello, friend.'
+var greeting = 'Hello, friend.';
 alert(greeting);
 
 // Ask user their name and store it to the variable username
@@ -23,22 +23,22 @@ console.log('The user\'s name is ' + username);
 
 // check user username for particular answers and return custom responses
 if (username.toLowerCase() === 'mr. robot' || username.toLowerCase() === 'mr robot') {
-    alert('What are you doing here?');
-    } else if (username.toLowerCase() === 'elliot') {
-        alert('Bonsoir, ' + username + '.');
-    } else if (username.toLowerCase() === 'sam sepiol') {
-        alert('I should look you up.');
-    } else if (username.toLowerCase() === 'lillian') {
-        alert('Hi ' + username + '! Please be kind in your grading!');
-    } else if (username.toLowerCase() === 'michelle') {
-        alert('Everyone look busy!');
-    } else if (username.toLowerCase() === 'eugene') {
-        alert('Careful with that ax, ' + username);
-    } else {
-    alert('Welcome to my site, ' + username + '.');
+  alert('What are you doing here?');
+} else if (username.toLowerCase() === 'elliot') {
+  alert('Bonsoir, ' + username + '.');
+} else if (username.toLowerCase() === 'sam sepiol') {
+  alert('I should look you up.');
+} else if (username.toLowerCase() === 'lillian') {
+  alert('Hi ' + username + '! Please be kind in your grading!');
+} else if (username.toLowerCase() === 'michelle') {
+  alert('Everyone look busy!');
+} else if (username.toLowerCase() === 'eugene') {
+  alert('Careful with that ax, ' + username);
+} else {
+  alert('Welcome to my site, ' + username + '.');
 }
 
-alert('Please answer either yes or no to the following questions.')
+alert('Please answer either yes or no to the following questions.');
 
 // Ask user how they feel and store it to the variable drFeelGood
 var drFeelGood = prompt('Are you feeling good today?');
@@ -46,11 +46,11 @@ console.log('The user\'s response to whether they are feeling good today was: ' 
 
 // Check drFeelGood variable for specific reponse
 if (drFeelGood.toLowerCase() === 'y' || drFeelGood.toLowerCase() === 'yes') {
-    alert('Glad to hear it, ' + username);
+  alert('Glad to hear it, ' + username);
 } else if (drFeelGood.toLowerCase() === 'n' || drFeelGood.toLowerCase() === 'no') {
-    alert('I\'m sorry ' + username + '. I hope you feel better soon!');
+  alert('I\'m sorry ' + username + '. I hope you feel better soon!');
 } else {
-  alert('You might want to go see a doctor, ' + username + '. You seem to be delirious')
+  alert('You might want to go see a doctor, ' + username + '. You seem to be delirious');
 }
 
 // Ask user if they have any money on them and store it to variable gotPaid
@@ -59,7 +59,7 @@ console.log('The user\'s response to whether they have cash on them was: ' + got
 
 // Check gotPaid variable for specific reponse
 if (gotPaid.toLowerCase() === 'y' || gotPaid.toLowerCase() === 'yes') {
-  var socialism = prompt('I\m running a bit light on cash at the moment. Could I borrow $20?');
+  var socialism = prompt('I\'m running a bit light on cash at the moment. Could I borrow $20?');
   if (socialism.toLowerCase() === 'y' || socialism.toLowerCase() === 'yes') {
     alert('Thanks ' + username + '! You\'re the best!');
   } else {
@@ -70,16 +70,16 @@ if (gotPaid.toLowerCase() === 'y' || gotPaid.toLowerCase() === 'yes') {
 } else {
   alert('Seriously let\'s get you to see a doctor, right away.');
 }
-// Ask user if it is time to get a drink and store response to variable beerO_Clock.
-var beerO_Clock = prompt('Well, it has certainly been a long day. Time to get a drink, ' + username + '?');
-console.log('The user\'s response to getting a drink was: ' + beerO_Clock);
+// Ask user if it is time to get a drink and store response to variable beerOclock.
+var beerOclock = prompt('Well, it has certainly been a long day. Time to get a drink, ' + username + '?');
+console.log('The user\'s response to getting a drink was: ' + beerOclock);
 
-// Check value of beerO_Clock and provide response based on that value
-if ((beerO_Clock.toLowerCase() === 'y' || beerO_Clock.toLowerCase() === 'yes') && (gotPaid.toLowerCase() === 'y' || gotPaid.toLowerCase() === 'yes')) {
+// Check value of beerOclock and provide response based on that value
+if ((beerOclock.toLowerCase() === 'y' || beerOclock.toLowerCase() === 'yes') && (gotPaid.toLowerCase() === 'y' || gotPaid.toLowerCase() === 'yes')) {
   alert('Sounds good to me! Let\'s hit up Brouwer\'s Cafe. You\'re buying!');
-} else if ((beerO_Clock.toLowerCase() === 'y' || beerO_Clock.toLowerCase() === 'yes') && (gotPaid.toLowerCase() !== 'y' || gotPaid.toLowerCase() !== 'yes')) {
+} else if ((beerOclock.toLowerCase() === 'y' || beerOclock.toLowerCase() === 'yes') && (gotPaid.toLowerCase() !== 'y' || gotPaid.toLowerCase() !== 'yes')) {
   alert('Great! I know a great place called Brouwer\'s Cafe that is nearby.');
-} else if ((beerO_Clock.toLowerCase() === 'n') || beerO_Clock.toLowerCase() === 'no') {
+} else if ((beerOclock.toLowerCase() === 'n') || beerOclock.toLowerCase() === 'no') {
   alert('Every party needs a pooper, that\'s why I\'m inviting you.  Party pooper, party pooper.');
 } else {
   alert('Actually, I think you already have had enough to drink.');
@@ -91,26 +91,26 @@ var areYouCool = prompt('Oh, hold up. You drink beer, right?');
 console.log('The user\'s response to whether they have cash on them was: ' + areYouCool);
 
 // Check value of areYouCool and provide response based on that value
-if ((beerO_Clock.toLowerCase() === 'y' || beerO_Clock.toLowerCase() === 'yes') && (gotPaid.toLowerCase() === 'y' || gotPaid.toLowerCase() === 'yes') 
+if ((beerOclock.toLowerCase() === 'y' || beerOclock.toLowerCase() === 'yes') && (gotPaid.toLowerCase() === 'y' || gotPaid.toLowerCase() === 'yes')
     && (areYouCool.toLowerCase() === 'y' || areYouCool.toLowerCase() === 'yes')) {
-      alert(username + ', I think this is the beginning of a beautiful friendship.');
-    } else if ((beerO_Clock.toLowerCase() === 'y' || beerO_Clock.toLowerCase() === 'yes') && (gotPaid.toLowerCase() !== 'y' || gotPaid.toLowerCase() !== 'yes') 
+  alert(username + ', I think this is the beginning of a beautiful friendship.');
+} else if ((beerOclock.toLowerCase() === 'y' || beerOclock.toLowerCase() === 'yes') && (gotPaid.toLowerCase() !== 'y' || gotPaid.toLowerCase() !== 'yes')
     && (areYouCool.toLowerCase() === 'y' || areYouCool.toLowerCase() === 'yes')) {
-      alert('You know, ' + username + '. You are alright in my book.');
-    } else if ((beerO_Clock.toLowerCase() === 'n' || beerO_Clock.toLowerCase() === 'no') && (areYouCool.toLowerCase() === 'n' || areYouCool.toLowerCase() === 'no')) {
-      alert('Well that explains it... Bye ' + username + '!');
-    } else if ((beerO_Clock.toLowerCase() === 'y' || beerO_Clock.toLowerCase() === 'yes') && (areYouCool.toLowerCase() === 'n' || areYouCool.toLowerCase() === 'no')) {
-      alert('Oh... You know, I just remember I\'ve got this thing I\'ve got to do right now... Bye ' + username + '!');
-    } else if (drunk === true && (areYouCool.toLowerCase() === 'n' || areYouCool.toLowerCase() === 'no')) {
-      alert('Uh huh. Sure you don\'t, ' + username + '.');
-    } else if ((beerO_Clock.toLowerCase() === 'n' || beerO_Clock.toLowerCase() === 'no') && (areYouCool.toLowerCase() === 'y' || areYouCool.toLowerCase() === 'yes')) {
-      alert('I just can\'t figure you out, ' + username + '.');
-    } else {
-      alert('Ok, ' + username +'. You\'re going to be alright. Just sit right down here and I will call 911');
-    }
+  alert('You know, ' + username + '. You are alright in my book.');
+} else if ((beerOclock.toLowerCase() === 'n' || beerOclock.toLowerCase() === 'no') && (areYouCool.toLowerCase() === 'n' || areYouCool.toLowerCase() === 'no')) {
+  alert('Well that explains it... Bye ' + username + '!');
+} else if ((beerOclock.toLowerCase() === 'y' || beerOclock.toLowerCase() === 'yes') && (areYouCool.toLowerCase() === 'n' || areYouCool.toLowerCase() === 'no')) {
+  alert('Oh... You know, I just remember I\'ve got this thing I\'ve got to do right now... Bye ' + username + '!');
+} else if (drunk === true && (areYouCool.toLowerCase() === 'n' || areYouCool.toLowerCase() === 'no')) {
+  alert('Uh huh. Sure you don\'t, ' + username + '.');
+} else if ((beerOclock.toLowerCase() === 'n' || beerOclock.toLowerCase() === 'no') && (areYouCool.toLowerCase() === 'y' || areYouCool.toLowerCase() === 'yes')) {
+  alert('I just can\'t figure you out, ' + username + '.');
+} else {
+  alert('Ok, ' + username + '. You\'re going to be alright. Just sit right down here and I will call 911');
+}
 
 // add event listener to mindGameLink so when it is clicked the function mindGame runs
-mindGameLink.addEventListener('click', mindGame); 
+mindGameLink.addEventListener('click', mindGame);
 
 // mindGame function with random number generator
 function mindGame() {
@@ -119,30 +119,30 @@ function mindGame() {
   if (guess === randomNum) {
     alert('Good guess ' + username + '! Have you been tested for psychic abilities?');
     console.log(guess + ' ' + randomNum + ' ' + i + 'psychic');
-  } else if (guess != randomNum) {
+  } else if (guess !== randomNum) {
     for(i = 1; i <= 4; i++) {
       guess = parseInt(guess);
       console.log(guess + ' ' + randomNum + ' ' + i + 'base');
       if ((guess === randomNum) && (i < 4)) {
         console.log(guess + ' ' + randomNum + ' ' + i + 'winner');
-        alert('Ding! Ding! Ding! We have a winner!')
+        alert('Ding! Ding! Ding! We have a winner!');
         break;
-      } else if ((guess > randomNum)  && (i < 4)) {
+      } else if ((guess > randomNum) && (i < 4)) {
         console.log(guess + ' ' + randomNum + ' ' + i + 'lower');
         guess = prompt('That\'s not it. The number I am looking for is lower. You are on attempt ' + (i + 1) + '.');
-      } else if ((guess < randomNum)  && (i < 4)) {
+      } else if ((guess < randomNum) && (i < 4)) {
         console.log(guess + ' ' + randomNum + ' ' + i + ' higher');
         guess = prompt('That\'s not it. The number I am looking for is higher. You are on attempt ' + (i + 1) + '.');
-      } else if ((guess == randomNum) && (i >= 4)) {
+      } else if ((guess === randomNum) && (i >= 4)) {
         console.log(guess + ' ' + randomNum + ' ' + i + ' nick');
         alert('You got it in just the nick of time!');
-      } else if ((guess != randomNum) && (i >= 4)) {
+      } else if ((guess !== randomNum) && (i >= 4)) {
         console.log(guess + ' ' + randomNum + ' ' + i + ' fortress');
         alert('Valiant effort but you were unable to penetrate my mind fortress! I was thinking of the number ' + randomNum + '.');
       } else if (isNaN(guess)) {
         console.log(guess + ' ' + randomNum + ' ' + i + 'bad player');
-          alert('That\'s not a number! No game for you!');
-          break;
+        alert('That\'s not a number! No game for you!');
+        break;
       } else {
         console.log(guess + ' ' + randomNum + ' ' + i + ' battleship');
         alert('You broke my battleship! Please tell me how you did that!');
@@ -181,7 +181,7 @@ function airForceFun(){
     soSmart--;
   }
 }
-// // Check value of stationed and provide response based on that value
+// Check value of stationed and provide response based on that value
 
 function stationedFun(){
   var stationed = prompt('I was stationed in Florida while in the Air Force.');
@@ -191,11 +191,11 @@ function stationedFun(){
   } else if (stationed.toLowerCase() === 'yes' || stationed.toLowerCase() === 'y') {
     alert('Nope, but I did grow up there. #FloridaMan');
   } else {
-      alert('Follow directions or lose points! You just lost 1');
-      soSmart--;
+    alert('Follow directions or lose points! You just lost 1');
+    soSmart--;
   }
 }
-// // Check value of university and provide response based on that value
+// Check value of university and provide response based on that value
 
 function universityFun(){
   var university = prompt('I studied Electronic Media at UTEP.');
@@ -209,7 +209,7 @@ function universityFun(){
     soSmart--;
   }
 }
-// // Check value of television and provide response based on that value
+// Check value of television and provide response based on that value
 
 function televisionFun(){
   var television = prompt('Did you know, I\'ve worked in television?');
@@ -223,10 +223,10 @@ function televisionFun(){
     soSmart--;
   }
 }
-// // Check value of spacey and provide response based on that value
+// Check value of spacey and provide response based on that value
 
 function spaceyFun(){
-  var spacey = prompt('My first program was in C# automating the calibration process for propulsion systems used in inter-galactic space travel.')
+  var spacey = prompt('My first program was in C# automating the calibration process for propulsion systems used in inter-galactic space travel.');
   if (spacey.toLowerCase() === 'no' || spacey.toLowerCase() === 'n') {
     alert('Right, it was just for DRABs for old pick-up trucks. But a guy can dream, can\'t he?');
     soSmart++;
@@ -237,7 +237,7 @@ function spaceyFun(){
     soSmart--;
   }
 }
-// // Check value of student and provide response based on that value
+// Check value of student and provide response based on that value
 
 function studentFun(){
   var student = prompt('Now, almost 20 years into my career, I\'m back to being a student at General Assembly, studying software development.');
@@ -251,10 +251,10 @@ function studentFun(){
     soSmart--;
   }
 }
-// // Check value of craftBeer looping through iLikeBeer array and provide response based on that value
+// Check value of craftBeer looping through iLikeBeer array and provide response based on that value
 
 function craftBeerFun(){
-  var craftBeer = prompt('"I drink beer. I like beer." Guess a style of beer that I like.')
+  var craftBeer = prompt('"I drink beer. I like beer." Guess a style of beer that I like.');
   for(i = 0; i < 6; i++) {
     if(iLikeBeer.includes(craftBeer.toLowerCase())){
       alert('Oh I do like those... You\'re making me thirsty.');
@@ -267,23 +267,23 @@ function craftBeerFun(){
     }
   }
 }
-// // Adds all values of iLikeBeer to the variable allMyBeers and adds a comma and space between each value 
+// Adds all values of iLikeBeer to the variable allMyBeers and adds a comma and space between each value
 function allMyBeersFun(){
   for(i = 0; i < iLikeBeer.length; i++) {
     if(i === iLikeBeer.length - 2) {
       allMyBeers = allMyBeers + iLikeBeer[i] + 's, and ';
     } else{
-    allMyBeers = allMyBeers + iLikeBeer[i] + 's, ';
+      allMyBeers = allMyBeers + iLikeBeer[i] + 's, ';
     }
   }
 
   // Removes the trailing space and comma from allMyBeers
 
   allMyBeers = allMyBeers.substring(0, allMyBeers.length - 2);
-  alert('My (probably not) complete list of favorite beer types are ' + allMyBeers + '.'); 
+  alert('My (probably not) complete list of favorite beer types are ' + allMyBeers + '.');
 }
 
-//   // Check value of soSmart and provide response based on that value
+// Check value of soSmart and provide response based on that value
 function soSmartFun(){
   if (soSmart === 7) {
     alert('You got all 7 answers right! What are you, some kind of stalker, ' + username + '?');
